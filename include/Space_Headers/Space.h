675b5ba0e83af01cb5a24e5f7a4dafb7a593371d
CursedRock17 <mtglucas1@gmail.com>
2022-11-18 21:54:37 -0500

    feat: added Space with 3D

---
+++ include/Space_Headers/Space.h
@@ -0,0 +1,18 @@
+#ifndef SPACE_H
+#define SPACE_H
+
+//Math libraries from opengl, needed for things like rotational matrices and dot products
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Space 
+{
+public:
+Space();
+~Space();
+
+private:
+};
+
+#endif
--- src/Graphics/vertex.glsl
+++ src/Graphics/vertex.glsl
@@ -6,8 +6,10 @@ layout (location = 2) in vec2 aTexture;
 out vec3 vertexColor;
 out vec2 textureCoordinate;
 
+uniform vec4 transform;
+
 void main(){
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = transform * vec4(aPos, 1.0);
     vertexColor = aColor;
     textureCoordinate = aTexture;
 }
